a161c40a2ebc693efcf4d09186e374c1afbee62c
Louis Panton <lpanton@purdue.edu>
2017-12-03 20:15:20 -0500

    Reverting main

--- ProceduralModeler/ProceduralModeler/main.cpp
+++ ProceduralModeler/ProceduralModeler/main.cpp
@@ -19,31 +19,31 @@ using namespace std;
 #include "GL/glut.h"
 #include "Cloud.h"
 #include "Terrain.h"
-#include "Tree.h"
 
 Terrain * t;
 Cloud * c;
 vector<Cloud *> cloudList;
-vector<Tree *> treeList;
 bool generatingMountains = true;
 bool generatingDesert = false;
 int leftEndpointY; //Holds value for height of leftEndpoint
 int rightEndpointY; //Holds value for height of rightEndpoint
 int leftEndpointX = 0; //Holds value for least x position (start of screen)
 int rightEndpointX = 500; //Holds value highest x position (width of screen)
-float roughnessFactor = 0.1; //Roughness Factor for calculating random variable, defined by user
+float roughnessFactor = 0.08; //Roughness Factor for calculating random variable, defined by user
 
-GLfloat modelMat[4][4] = {
-	{ 1, 0, 0, 0 },
-	{ 0, 1, 0, 0 },
-	{ 0, 0, 1, 0 },
-	{ 0, -1, -26, 1 }
-};
+int winHeight = 500;
+int winWidth = 500;
+int scale = 4;
+int cols = 1024 / scale;
+int rows = 1024 / scale;
+
+int lightX = 0;
+int lightY = 0;
 
 
 void drawTerrain()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0.0, 500.0, 500.0, 0.0);
@@ -55,28 +55,115 @@ void drawTerrain()
 			float pixNoise = t->pixels[i][j];
 			glBegin(GL_POINTS);
 			if (pixNoise == 1.0) {
-				if (j < 100)
+				if (j < 125)
 				{
-					glColor3f(1.0, 1.0, 1.0);
+					int gray = 1;
+					if (j > 110)
+					{
+						gray = rand() % 2;
+					}
+					else if (j > 108)
+					{
+						gray = rand() % 5;
+					}
+					else if (j > 100)
+					{
+						gray = rand() % 10;
+					}
+					if (gray == 0)
+					{
+						glColor3f(0.5, 0.5, 0.5);
+					}
+					else
+					{
+						glColor3f(1.0, 1.0, 1.0);
+					}
 				}
-				else if (j < 250)
+				else if (j < 300)
 				{
-					glColor3f(0.5, 0.5, 0.5);
+					int white = 1;
+					int green = 1;
+					if (j < 130)
+					{
+						white = rand() % 2;
+					}
+					else if (j < 140)
+					{
+						white = rand() % 5;
+					}
+					else if (j < 150)
+					{
+						white = rand() % 10;
+					}
+
+					if (j > 290)
+					{
+						green = rand() % 2;
+					}
+					else if (j > 280)
+					{
+						green = rand() % 5;
+					}
+					else if (j > 275)
+					{
+						green = rand() % 10;
+					}
+					if (white == 0)
+					{
+						glColor3f(1.0, 1.0, 1.0);
+					}
+					else if (green == 0)
+					{
+						glColor3f(0.0, 1.0, 0.0);
+					}
+					else
+					{
+						glColor3f(0.5, 0.5, 0.5);
+					}
 				}
 				else
 				{
 					if (generatingDesert)
 					{
-						glColor3f(0.86, 0.58, 0.44);
+						int darkBrown = rand() % 15;
+						if (darkBrown == 0)
+						{
+							glColor3f(0.36, 0.25, 0.31);
+						}
+						else
+						{
+							glColor3f(0.86, 0.58, 0.44);
+						}
 					}
-					else {
-						glColor3f(0.0, 1.0, 0.0);
+					else
+					{
+						int gray = 1;
+						if (j < 310)
+						{
+							gray = rand() % 2;
+						}
+						else if (j < 320)
+						{
+							gray = rand() % 5;
+						}
+						else if (j < 325)
+						{
+							gray = rand() % 10;
+						}
+						if (gray == 0)
+						{
+							glColor3f(0.5, 0.5, 0.5);
+						}
+						else
+						{
+							glColor3f(0.0, 1.0, 0.0);
+						}
 					}
 				}
 			}
 			else
 			{
-				glColor3f(0.0, 0.5, 0.8);
+				glColor3f(0.2, 0.6, 0.8);
 			}
 			glVertex2i(i, j);
 			glEnd();
@@ -135,34 +222,32 @@ void drawClouds()
 	glFlush();
 }
 
-void genTree(float x, float y, float z)
+GLfloat myModelMat[4][4] =
 {
-	Tree * tree;
-	tree = new Tree(x, y, z);
-	tree->genBranches(tree);
-	treeList.push_back(tree);
-}
+	{1, 0, 0, 0},
+	{0, 1, 0, 0},
+	{0, 0, 1, 0},
+	{-525, -200, -1200, 1}
+};
 
-void drawTrees()
+//GLfloat ambient[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+//GLfloat diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f};
+//GLfloat specular[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+//GLfloat position[] = {-500.0f, -500.0f, -1000.0f, 1.0f };
+
+void drawTerrain3D()
 {
-	float fovy = 90.0;
-	float aspect = 500.0 / 500.0;
-	float zNear = 1.0;
-	float zFar = 100.0;
-
-	//perspective projection
-	/* Clear the window */
+	glClearColor(0.2, 0.6, 0.8, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(fovy, aspect, zNear, zFar);
-
+	gluPerspective(30, 1, 1, -500);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	glLoadMatrixf((GLfloat *)modelMat);
+	glLoadMatrixf((GLfloat *)myModelMat);
 
+	glEnableClientState(GL_VERTEX_ARRAY);
 
-	//set up lighting effects
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 
@@ -170,10 +255,10 @@ void drawTrees()
 	glEnable(GL_COLOR_MATERIAL);
 
 	// Create light components
-	float ambientLight[] = { 0.8, 0.8, 0.8, 1.0 };
+	float ambientLight[] = { 0.6, 0.6, 0.6, 1.0 };
 	float diffuseLight[] = { 0.8, 0.8, 0.8, 1.0 };
 	float specularLight[] = { 0.5, 0.5, 0.5, 1.0 };
-	float position[] = { 1.0, 1.0, 0.0, 1.0 };
+	float position[] = { 500, 400, 1100, 1.0 };
 
 	// Assign created components to GL_LIGHT0
 	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
@@ -181,105 +266,92 @@ void drawTrees()
 	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
 	glLightfv(GL_LIGHT0, GL_POSITION, position);
 
-	
+	int color;
 
-
-	//for each tree
-	for (int t = 0; t < treeList.size(); t++)
+	for (int z = 1; z < rows; z++)
 	{
-		Tree * tree = treeList.at(t);
-		vector <Tree *> branchList = tree->branches;
-		
-		//for each branch in a tree
-		for (int a = 0; a < branchList.size(); a++)
+		glBegin(GL_TRIANGLE_STRIP);
+		for (int x = 1; x < cols; x++)
 		{
-			//get a branch
-			Tree * branch = branchList.at(a);
-
-			//get angles
-			float angleX = branch->angleX;
-			float angleY = branch->angleY;
-			float angleZ = branch->angleZ;
-
-			//get location
-			float x = branch->x;
-			float y = branch->y;
-			float z = branch->z;
-			//get radius and height
-			float radiusB = branch->radiusBottom;
-			float radiusT = branch->radiusTop;
-			float height = branch->height;
-			//create a cylinder
-			GLUquadric *obj = gluNewQuadric();
-			//brown color
-			glColor3f(0.5, 0.35, 0.1);
-			//if smallest branch, make it a leaf
-			if (branch->height <= 0.0)
+			color = rand() % 2;
+			if (color == 0)
 			{
-				glPushMatrix();
-
-				//position just above tallest branches
-				glTranslatef(x, y - 1 + 0.1, z);
-
-				//flip
-				glScalef(1.0f, -1.0f, 1.0f);
-				glColor3f(0.5, 0.6 , 0.1);
-				
-				glRotated(angleX, 1, 0, 0);
-				glRotated(angleY, 0, 1, 0);
-				glRotated(angleZ, 0, 0, 1);
-
-				//make leaves as spheres
-				gluSphere(obj, 20*radiusB, 30, 30);
-				glPopMatrix();
+				glColor3f(0.137255, 0.556863, 0.137255);
 			}
-			//if tall enough make a branch
-			else
+			else if (color == 1)
 			{
-
-				glPushMatrix();
-
-				//position
-				glTranslatef(x, y - 1, z);
-
-				//flip
-				glScalef(1.0f, -1.0f, 1.0f);
-
-				glRotated(angleX, 1, 0, 0);
-				glRotated(angleY, 0, 1, 0);
-				glRotated(angleZ, 0, 0, 1);
-
-				//tappered cylinder 
-				gluCylinder(obj, radiusB, radiusT, height, 30, 30);
-				glPopMatrix();
+				glColor3f(0.184314, 0.309804, 0.184314);
 			}
-
+			glNormal3f(0.0f, 0.0f, 1.0f);
+			glVertex3f(x *scale, t->terrain[z * scale][x * scale], z * scale);
+			glVertex3f((x + 1) * scale, t->terrain[z * scale][(x + 1) * scale], z * scale);
+			glVertex3f(x * scale, t->terrain[(z + 1) * scale][x * scale], (z + 1) * scale);
 		}
-		
+		glEnd();
 	}
-	
-	//disable lighting effects and flush buffers
+
+	for (int z = 1; z < rows; z++)
+	{
+		glBegin(GL_TRIANGLE_STRIP);
+		for (int x = 1; x < cols; x++)
+		{
+			color = rand() % 2;
+			if (color == 0)
+			{
+				glColor3f(0.137255, 0.556863, 0.137255);
+			}
+			else if (color == 1)
+			{
+				glColor3f(0.184314, 0.309804, 0.184314);
+			}
+			glNormal3f(0.0f, 0.0f, 1.0f);
+			glVertex3f((x + 1) * scale, t->terrain[z * scale][(x + 1) * scale], z * scale);
+			glVertex3f(x * scale, t->terrain[(z + 1) * scale][x * scale], (z + 1) * scale);
+			glVertex3f((x + 1) * scale, t->terrain[(z + 1) * scale][(x + 1) * scale], (z + 1) * scale);
+		}
+		glEnd();
+	}
+
+	//for (int z = 1; z < rows; z++)
+	//{
+	//	glBegin(GL_LINES);
+	//	for (int x = 1; x < cols; x++)
+	//	{
+	//		glVertex3f(x*scale, t->terrain[z*scale][x*scale], z*scale);
+	//		glVertex3f((x+1)*scale, t->terrain[z*scale][(x+1)*scale], z*scale);
+	//	}
+	//	glEnd();
+	//}
+
+	//for (int z = 1; z < rows; z++)
+	//{
+	//	glBegin(GL_LINES);
+	//	for (int x = 1; x < cols; x++)
+	//	{
+	//		glVertex3f(x*scale, t->terrain[z*scale][x*scale], z*scale);
+	//		glVertex3f(x*scale, t->terrain[(z+1)*scale][x*scale], (z+1)*scale);
+	//	}
+	//	glEnd();
+	//}
 	glDisable(GL_LIGHT0);
 	glDisable(GL_LIGHTING);
 	glDisable(GL_COLOR_MATERIAL);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	
 	glutSwapBuffers();
 	glFlush();
-
 }
 
 void display()
 {
 	glClearColor(0, 0, 0, 1);
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	//Split area by 2 every time until we cover the whole thing
-	//Calculate the midpoint at each step
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	/*Draw Clouds comment out if need be*/
 	//drawClouds();
 
 	//drawTerrain();
-	drawTrees();
+	drawTerrain3D();
 }
 
 /*redraw display*/
@@ -307,14 +379,9 @@ int main(int argc, char **argv)
 	c = new Cloud();
 	cloudList.push_back(c);
 
-	//generate a tree
-	genTree(-0.2,0.0,20.0);
-
-	genTree(0.2, 0.0, 25.0);
-
     /* Initialize the GLUT window */
     glutInit(&argc, argv);
-    glutInitWindowSize(500, 500);
+	glutInitWindowSize(winWidth, winHeight);
     glutInitWindowPosition(30, 30);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     glutCreateWindow("CS 334 - Procedural Modeling");
@@ -323,13 +390,21 @@ int main(int argc, char **argv)
 	t = new Terrain(rightEndpointX, generatingMountains);
 
 	//need something to start with what kind of terrain we're generating
-	t->generateEndpoints();
+//	t->generateEndpoints();
 	//calculate values, store in array
-	t->calcMidpoints(leftEndpointX, t->heights[leftEndpointX], rightEndpointX, t->heights[rightEndpointX], roughnessFactor);
-	t->makePicture();
+//	t->calcMidpoints(leftEndpointX, t->heights[leftEndpointX], rightEndpointX, t->heights[rightEndpointX], roughnessFactor);
+//	t->makePicture();
+
+	t->generateEndpoints3D();
+	t->TerrainGenerate(1024);
+//	t->printHeights();
+
+	glEnable(GL_NORMALIZE);
+
+
 
 	glutDisplayFunc(display);
-	glutIdleFunc(idle);
+//	glutIdleFunc(idle);
 
 	/* Init GLEW */
 	GLenum err = glewInit();
